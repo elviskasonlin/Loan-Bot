d02395b3ea2ee0e4d7d7dedebb3ae4cef53eca5c
elviskasonlin <elviskason.lin@gmail.com>
2018-10-09 17:17:41 +0700

    build

--- src/index.js
+++ src/index.js
@@ -2,12 +2,53 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
 app.use(require('body-parser').json());
 
-app.use(function(req, res) {
-  res.status(404).send('Undefined route');
+app.listen(process.env.PORT, () => {
+  console.log("Webhook is listening on port " + process.env.PORT);
 });
 
-console.log("success");
-app.listen(process.env.PORT);
+// Webhook endpoint
+app.post('/webhook', (req, res) => {
+  let body = req.body;
+
+   if (body.object === 'page') {
+    body.entry.forEach(function(entry) {
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+app.get('/webhook', (req, res) => {
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+      
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
