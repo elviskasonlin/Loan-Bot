f3e06a2e8021dddc9fdbebde5a3401a58812cf29
elviskasonlin <elviskason.lin@gmail.com>
2018-10-10 11:52:41 +0700

    Basic webhook functionality

--- src/index.js
+++ src/index.js
@@ -6,11 +6,17 @@ const express = require('express');
 const app = express();
 app.use(require('body-parser').json());
 
+const { VERIFY_TOKEN } = process.env.VERIFY_TOKEN;
+
 app.listen(process.env.PORT, () => {
   console.log("Webhook is listening on port " + process.env.PORT);
 });
 
-// Webhook endpoint
+/////////////////////
+// Webhook endpoint//
+/////////////////////
+
+// POST method route for /webhook 
 app.post('/webhook', (req, res) => {
   let body = req.body;
 
@@ -29,10 +35,8 @@ app.post('/webhook', (req, res) => {
 
 });
 
+// GET method route for /webhook
 app.get('/webhook', (req, res) => {
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-      
   // Parse the query params
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
@@ -40,14 +44,11 @@ app.get('/webhook', (req, res) => {
     
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-
     // Checks the mode and token sent is correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-    
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);      
