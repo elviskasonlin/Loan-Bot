8fcc3939bd0b6f0b6b46cf82a1861b29e3426b4f
elviskasonlin <elviskason.lin@gmail.com>
2018-10-11 16:19:35 +0700

    build

--- src/loanApp/index.js
+++ src/loanApp/index.js
@@ -3,9 +3,18 @@ const questions = require('./questions.json');
 const ocr = require('../ocr/ocr');
 
 module.exports.start = function(body) {
-  module.exports.state.report[event.sender.id] = {
-    question: questions[0];
+  module.exports.state.report[body.sender.id] = {
+    question: questions[1]
   }
 
+  graph.sendMessage(body.sender.id, { 
+    "text" : "You can type 'quit' anytime to stop the loan application process."
+  })
+
   ocr.ocr("http://gdurl.com/UzMJ");
+}
+
+
+const askQuestion = function(event, question) {
+
 }
