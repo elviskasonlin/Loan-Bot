1e2e64499ca509ef5b7fc4edf6c36adec407bdd2
elviskasonlin <elviskason.lin@gmail.com>
2018-10-11 13:21:46 +0700

    build

--- src/loanApp/index.js
+++ src/loanApp/index.js
@@ -1,6 +1,6 @@
 const graph = require('../graph/index');
 const questions = require('./questions.json');
 
-module.exports.start = function(body) {
-  graph.sendMessage(body.sender.id, {"text": "LOANAPP START INVOKED"});
-}
+// module.exports.start = function(body) {
+//   graph.sendMessage(body.sender.id, {"text": "LOANAPP START INVOKED"});
+// }
