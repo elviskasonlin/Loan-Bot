cd3d1d028d89e090da10f2e553d66429421ca048
elviskasonlin <elviskason.lin@gmail.com>
2018-10-12 16:46:20 +0700

    build

--- src/ocr/ocr.js
+++ src/ocr/ocr.js
@@ -10,20 +10,35 @@ const classify = require('./classify');
 // OCR Handler //
 /////////////////
 
-module.exports.ocr = function (image_uri) {
+module.exports.ocr = function(image_uri) {
 
   console.log("OCR HANDLER CALLED");
   // Creates a client for ocr
-  const client = new gvision.ImageAnnotatorClient();
-
-  // Performs label detection on the image file
   client
-  .documentTextDetection(image_uri)
-  .then(results => {
-    const fullTextAnnotation = results[0].fullTextAnnotation;
-    console.log(`Full text: ${fullTextAnnotation.text}`);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
+    .documentTextDetection(image_uri)
+    .then(results => {
+      const fullTextAnnotation = results[0].fullTextAnnotation;
+      console.log(`Full text: ${fullTextAnnotation.text}`);
+
+      fullTextAnnotation.pages.forEach(page => {
+        page.blocks.forEach(block => {
+          console.log(`Block confidence: ${block.confidence}`);
+          block.paragraphs.forEach(paragraph => {
+            console.log(`Paragraph confidence: ${paragraph.confidence}`);
+            paragraph.words.forEach(word => {
+              const wordText = word.symbols.map(s => s.text).join('');
+              console.log(`Word text: ${wordText}`);
+              console.log(`Word confidence: ${word.confidence}`);
+              word.symbols.forEach(symbol => {
+                console.log(`Symbol text: ${symbol.text}`);
+                console.log(`Symbol confidence: ${symbol.confidence}`);
+              });
+            });
+          });
+        });
+      });
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
 };
