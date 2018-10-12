9a912a5a7a6868cc3f5c045ed2304f7f77fcf07e
elviskasonlin <elviskason.lin@gmail.com>
2018-10-12 16:58:04 +0700

    build

--- src/ocr/ocr.js
+++ src/ocr/ocr.js
@@ -4,54 +4,30 @@
 
 const gvision = require('@google-cloud/vision');
 const gfirestore = require('@google-cloud/firestore');
-const classify = require('./classify');
 
 /////////////////
 // OCR Handler //
 /////////////////
 
-module.exports.ocr = function(image_uri) {
-
-  console.log("OCR HANDLER CALLED");
+module.exports.ocr = function (image_uri) {
   // Creates a client for ocr
   const client = new gvision.ImageAnnotatorClient();
 
-  // Send & process
-  // client
-  //   .documentTextDetection(image_uri)
-  //   .then(results => {
-  //     const fullTextAnnotation = results[0].fullTextAnnotation;
-  //     console.log(`Full text: ${fullTextAnnotation.text}`);
-
-  //     fullTextAnnotation.pages.forEach(page => {
-  //       page.blocks.forEach(block => {
-  //         console.log(`Block confidence: ${block.confidence}`);
-  //         block.paragraphs.forEach(paragraph => {
-  //           console.log(`Paragraph confidence: ${paragraph.confidence}`);
-  //           paragraph.words.forEach(word => {
-  //             const wordText = word.symbols.map(s => s.text).join('');
-  //             console.log(`Word text: ${wordText}`);
-  //             console.log(`Word confidence: ${word.confidence}`);
-  //             word.symbols.forEach(symbol => {
-  //               console.log(`Symbol text: ${symbol.text}`);
-  //               console.log(`Symbol confidence: ${symbol.confidence}`);
-  //             });
-  //           });
-  //         });
-  //       });
-  //     });
-  //   })
-  //   .catch(err => {
-  //     console.error('ERROR:', err);
-  //   });
-    client
+  // Performs label detection on the image file
+  client
     .textDetection(image_uri)
     .then(results => {
-      const detections = results[0].textAnnotations;
-      console.log('Text:');
-      detections.forEach(text => console.log(text));
+      // Almost entire object.
+      // detections include bounding boxes etc.
+      // text only accesses the final stringed text
+      const detections = results[0].fullTextAnnotation;
+      const text = detections.text;
+
+      // console.log('Text:' + text);
+      console.log(detections);
     })
     .catch(err => {
       console.error('ERROR:', err);
     });
+
 };
